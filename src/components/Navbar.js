2b02ec27a6ef6f274316aae7912758a941394a3b
Brune <abrune.osteo@gmail.com>
2022-05-18 23:38:20 +0200

    update from scrimba course for dark/light toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
-import React from "react"
-import reactLogo from "../images/react-icon-small.png"
+import React from "react";
+import reactLogo from "../images/react-icon-small.png";
 
-export default function Navbar() {
-    return (
-        <nav>
-            <img src={reactLogo} alt="React Logo" className="nav--icon" />
-            <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
-        </nav>
-    )
+export default function Navbar(props) {
+  return (
+    <nav className={props.darkMode ? "dark" : ""}>
+      <img src={reactLogo} alt="React Logo" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+    </nav>
+  );
 }
