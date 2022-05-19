902fa3e7d1bf03c7d83637a8c5211b1462a33646
Brune <abrune.osteo@gmail.com>
2022-05-19 12:37:49 +0200

    added light/darkmode feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,22 +2,18 @@ import React from "react";
 import reactLogo from "../images/react-icon-small.png";
 
 export default function Navbar(props) {
+
   return (
     <nav className={props.darkMode ? "dark" : ""}>
       <img src={reactLogo} alt="React Logo" className="nav--icon" />
       <h3 className="nav--logo_text">ReactFacts</h3>
-      <div 
-                className="toggler" 
-            >
-                <p className="toggler--light">Light</p>
-                <div 
-                    className="toggler--slider"
-                    onClick={props.toggleDarkMode}
-                >
-                    <div className="toggler--slider--circle"></div>
-                </div>
-                <p className="toggler--dark">Dark</p>
-            </div>
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
   );
 }
